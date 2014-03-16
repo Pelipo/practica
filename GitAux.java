ec3a7d96ce2a96feb90adcb21589df7b1b317ad9
Arkadio <195912@student.pwr.wroc.pl>
2014-03-16 23:28:08 +0100

    Modificacion alumno2

--- GitAux.java
+++ GitAux.java
@@ -16,11 +16,13 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+// modificaciones alumno2
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
-    }
+// modificaciones alumno2  
+  }
   public void testGitAux(){      // metodo rama testing  System.out.println("Metodo de testing");     }
 }
 // comentario incluido
